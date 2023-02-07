bb70e3a60d135e0e51cccd3307bd98d3a01a4192
danimunf <akhmad.dani@efishery.com>
2023-02-07 08:41:38 +0700

    update say hello with parameter name

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/gaisuke/go-belajar-module
+module github.com/gaisuke/go-belajar-module/v2
 
 go 1.19
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,5 @@
 package gobelajarmodule
 
-func SayHello() string {
-	return "Hello World"
+func SayHello(name string) string {
+	return "Hello " + name
 }
